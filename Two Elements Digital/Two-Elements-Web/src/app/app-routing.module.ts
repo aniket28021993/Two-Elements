ebc6e03703f8cb7743a6c5057977de0bc18c2847
aniket dalvi <aniket dalvi@DESKTOP-592O47S>
2026-07-17 17:58:47 +0530

    commit

--- Two Elements Digital/Two-Elements-Web/src/app/app-routing.module.ts
+++ Two Elements Digital/Two-Elements-Web/src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { PublicLayoutComponent } from './layouts/public-layout/public-layout.component';
+import { HomeComponent } from './pages/home/home.component';
+
+const routes: Routes = [
+
+  {
+    path: '',
+    component: PublicLayoutComponent,
+    children: [
+      {
+        path: '',
+        component: HomeComponent
+      }
+    ]
+  },
+
+  {
+    path: '**',
+    redirectTo: ''
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Two Elements Digital/Two-Elements-Web/src/app/app.module.ts
+++ Two Elements Digital/Two-Elements-Web/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PublicLayoutComponent } from './layouts/public-layout/public-layout.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PublicLayoutComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
